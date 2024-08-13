3140af1038871a8117aff5128b49d88bc762adff
soapgu <ghost_gu@163.com>
2024-08-13 16:46:13 +0800

    feat: 🎸 demo query matrix mixer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # biamp-client-demo
 
 npm install /Users/guhui/Githubs/biamp-client
+npm uninstall /Users/guhui/Githubs/biamp-client
+
+npm install @shgbit/biamp-client
+npm uninstall @shgbit/biamp-client
--- index.js
+++ index.js
@@ -10,11 +10,24 @@ const main = async () => {
     const client = new biampClient('172.16.13.52','main');
     console.log("<<<<<<< begin connect biamp >>>>>>>");
     let connect = await client.makeConnect();
-    console.log(`<<<<<<< end connect biamp,result${connect} >>>>>>>`);
+    console.log(`<<<<<<< end connect biamp,result:${connect} >>>>>>>`);
     if( connect ){
         console.log("<<<<<<< begin get serial number >>>>>>>");
         let sn = await client.sendTTPSimple( DeviceBlock.getSerialNumber());
         console.log(`serial number is ${sn}`);
+
+        console.log("<<<<<<< begin get MatrixMixer state >>>>>>>");
+
+        const mixer = new MatrixMixerBlock('Mixer1');
+        let inputNum = await client.sendTTPSimple( mixer.getInputCount() );
+        let outputNum =  await client.sendTTPSimple(mixer.getOutputCount());
+        console.log(`MatrixMixer is input:${inputNum} ✖️ output:${outputNum}`);
+        for( let i=1;i<= inputNum;i++ ){
+            for( let j=1;j <= outputNum;j++ ){
+                let state = await client.sendTTPSimple( mixer.getCrosspointLevelState( i ,j ) );
+                console.log(`input:${i} output:${j} queryMixerState result:${state}`);
+            }
+        }
     }
     console.log(`end for main`);
     
