9ea75bd4f3c056653811699c57b87fc0ca4bd5de
soapgu <ghost_gu@163.com>
2024-08-14 15:25:05 +0800

    feat: 🎸 add batchQueryCrosspointLevelState

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # biamp-client-demo
 
+local
+```
 npm install /Users/guhui/Githubs/biamp-client
 npm uninstall /Users/guhui/Githubs/biamp-client
+```
 
+remote
+```
 npm install @shgbit/biamp-client
 npm uninstall @shgbit/biamp-client
+```
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const biampClient = require('@shgbit/biamp-client');
+const BiampClient = require('@shgbit/biamp-client');
 
-const { DeviceBlock,MatrixMixerBlock } = biampClient;
+const { DeviceBlock,MatrixMixerBlock } = BiampClient;
 
 
 
 
 const main = async () => {
 	console.log("---main begin---");
-    const client = new biampClient('172.16.13.52','main');
+    const client = new BiampClient('172.16.31.52','main');
     console.log("<<<<<<< begin connect biamp >>>>>>>");
     let connect = await client.makeConnect();
     console.log(`<<<<<<< end connect biamp,result:${connect} >>>>>>>`);
@@ -22,16 +22,57 @@ const main = async () => {
         let inputNum = await client.sendTTPSimple( mixer.getInputCount() );
         let outputNum =  await client.sendTTPSimple(mixer.getOutputCount());
         console.log(`MatrixMixer is input:${inputNum} ✖️ output:${outputNum}`);
-        for( let i=1;i<= inputNum;i++ ){
-            for( let j=1;j <= outputNum;j++ ){
-                let state = await client.sendTTPSimple( mixer.getCrosspointLevelState( i ,j ) );
-                console.log(`input:${i} output:${j} queryMixerState result:${state}`);
-            }
-        }
+        await normalQueryCrosspointLevelState( client,mixer,inputNum,outputNum );
+        await batchQueryCrosspointLevelState(client,mixer,inputNum,outputNum );
     }
     console.log(`end for main`);
-    
 };
+
+/**
+ * 
+ * @param {BiampClient} client 
+ * @param {MatrixMixerBlock} mixer 
+ * @param {number} inputNum 
+ * @param {number} outputNum 
+ */
+async function normalQueryCrosspointLevelState(client,mixer,inputNum,outputNum){
+    const start = process.hrtime.bigint();
+    for( let i=1;i<= inputNum;i++ ){
+        for( let j=1;j <= outputNum;j++ ){
+            let state = await client.sendTTPSimple( mixer.getCrosspointLevelState( i ,j ) );
+            console.log(`input:${i} output:${j} queryMixerState result:${state}`);
+        }
+    }
+    const end = process.hrtime.bigint();
+    const elapsedTime = end - start;
+    const elapsedMilliseconds = Number(elapsedTime) / 1_000_000;
+    console.log(`normal query CrosspointLevelState timming: ${elapsedMilliseconds} ms`);
+}
+
+/**
+ * 
+ * @param {BiampClient} client 
+ * @param {MatrixMixerBlock} mixer 
+ * @param {number} inputNum 
+ * @param {number} outputNum 
+ */
+async function batchQueryCrosspointLevelState(client,mixer,inputNum,outputNum) {
+    const start = process.hrtime.bigint();
+    let requests = [];
+    for( let i=1;i<= inputNum;i++ ){
+        for( let j=1;j <= outputNum;j++ ){
+            let request = mixer.getCrosspointLevelState( i ,j );
+            requests.push( request );
+        }
+    }
+    console.log(`client request count:${requests.length}`);
+    let results =  await client.sendTTPSimple(...requests);
+    console.log(results)
+    const end = process.hrtime.bigint();
+    const elapsedTime = end - start;
+    const elapsedMilliseconds = Number(elapsedTime) / 1_000_000;
+    console.log(`batch query CrosspointLevelState timming: ${elapsedMilliseconds} ms`);
+}
 
 main().catch(error => {
 	console.error('============= main catched error:', error);
